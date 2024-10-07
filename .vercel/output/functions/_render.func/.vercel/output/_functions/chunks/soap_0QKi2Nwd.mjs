8aad344d51e3a74964c5272dd7a85d7cfb52ce57
Shardendu Mishra <shardendumishra01@gmail.com>
2024-10-08 01:58:53 +0530

    Please Maar Do Mujhe

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/soap_0QKi2Nwd.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./soap_Dq9T0w2l.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +9,5 @@ export default defineConfig({
     tailwind(),
     react(),
   ],
-  adapter: vercel(),
   output : "server"
 });
